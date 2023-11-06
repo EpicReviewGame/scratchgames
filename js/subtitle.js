d044a725c67cd984114a629c3663bbebdea0e8b1
EpicReviewGame <118922548+EpicReviewGame@users.noreply.github.com>
2023-11-06 18:10:51 -0500

    Update subtitle.js

--- js/subtitle.js
+++ js/subtitle.js
@@ -6,13 +6,20 @@ function changeSub(num) {
 
 
 var say = [];
-say[0] = "Remember kids: Sylveontheanimator is mentally ill! =)";
+say[0] = "don't look up @sylviexd on youtube, worst mistake of my life";
 say[1] = "7/31/23 - the death of nonexisty bfdi";
 say[2] = "it won't fit!";
 say[3] = "i think i won this trend";
-say[4] = "7/31/23 - the death of nonexisty bfdi";
-say[5] = "7/31/23 - the death of nonexisty bfdi";
-say[6] = "7/31/23 - the death of nonexisty bfdi";
+say[4] = "my trumpet failed on me";
+say[5] = "i forgot my trumpet";
+say[6] = "you make fun of everything i fricking say!!11";
+say[7] = "[...] keeps stealing things outta my pocket!!!";
+say[8] = "real";
+say[9] = "what the frick";
+say[10] = "shut the farts up";
+say[11] = "you are literally a dumbass";
+say[12] = "that darn donkey kong";
+say[13] = "ah ah go crazy!!!!";
 
 // pick a random greeting
 var howmany = say.length;
